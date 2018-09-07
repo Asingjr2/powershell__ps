9b9fbbd57f34131bc2ce3db90b5926c33c4f0446
asingjr2 <asingjr2@gmail.com>
2018-09-07 18:17:15 -0400

    adding beginnings or scripts

---
+++ scripts/helloWorld.ps1
@@ -0,0 +1,9 @@
+﻿# First basic script in powershell.
+# Will not run in cmdline.
+# Easier to write in ISE.
+
+
+# Create variable
+$greeting = "Hello Kind Sir or Ma'am"
+# Cmdlet to write to screen
+Write-Host $greeting
